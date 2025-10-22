c53f7c8482bf990382c3a724db6ad279b2e98e28
Manav Goyal <manucool123.rich@gmail.com>
2025-10-22 19:40:04 +0530

    refactor: update main entry point to use new styling

--- algointent/projects/algointent/src/main.tsx
+++ algointent/projects/algointent/src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/tailwind.css'
-import './styles/App.css'
-import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
 )
