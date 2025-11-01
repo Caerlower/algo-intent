a8c2139f182483274e2f232731337afc26a44bb4
Manav Goyal <manucool123.rich@gmail.com>
2025-11-02 01:58:55 +0530

    feat: add routing for landing, features, docs and about pages

--- algointent/projects/algointent/src/App.tsx
+++ algointent/projects/algointent/src/App.tsx
@@ -4,11 +4,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
 import Index from "./pages/Index";
+import Landing from "./pages/Landing";
+import Features from "./pages/Features";
+import Documentation from "./pages/Documentation";
+import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { getAlgodConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
+import { SocialWalletProvider } from './providers/EnhancedWalletProvider'
 
 const queryClient = new QueryClient();
 
@@ -20,6 +27,29 @@ let supportedWallets: SupportedWallet[]
     // If you are interested in WalletConnect v2 provider
     // refer to https://github.com/TxnLab/use-wallet for detailed integration instructions
   ]
+
+function Layout() {
+  const location = useLocation();
+  const isAppRoute = location.pathname === "/app";
+
+  return (
+    <>
+      {!isAppRoute && <Navbar />}
+      
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/documentation" element={<Documentation />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/app" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      
+      {!isAppRoute && <Footer />}
+    </>
+  );
+}
 
 export default function App() {
   const algodConfig = getAlgodConfigFromViteEnvironment()
@@ -45,17 +75,15 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
         <TooltipProvider>
-                <SnackbarProvider maxSnack={3} style={{ zIndex: 9999999 }}>
+          <SnackbarProvider maxSnack={3} style={{ zIndex: 9999999 }}>
             <WalletProvider manager={walletManager}>
-              <Toaster />
-              <Sonner />
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </BrowserRouter>
+              <SocialWalletProvider>
+                <Toaster />
+                <Sonner />
+                <BrowserRouter>
+                  <Layout />
+                </BrowserRouter>
+              </SocialWalletProvider>
             </WalletProvider>
           </SnackbarProvider>
         </TooltipProvider>
